019c62c0b1be769b4711e9ed4afeb6b490347ba9
root <root@Kazmaros>
2023-12-11 13:52:24 +1000

    initial files, components and pages set up

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WeatherProvider>
+        <NavMenu />
+        
+        <Forecast />
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+
+              {/* Nested routes set up as-is from the Ed lesson! */}
+              <Route path="/articles" element={<Outlet />}>
+              <Route index element={<Articles />} />
+              <Route path=":postcode" element={<Page />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
+        <Radar />
+      </WeatherProvider>
     </div>
   );
 }
