0b4931ceeeefc72ba991abfb57ff8fbc8ef483e1
TheoMarentes <theokazmaros@gmail.com>
2023-12-11 15:29:07 +1000

    routing and layout changes, added dynamic locations page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import { LocationSearch } from './components/LocationSearch';
+import { HomePage } from './pages/HomePage'
+import { LocationPage } from './pages/LocationPage'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path=":city" element={<LocationPage />} />
+      </Routes>
+	  </BrowserRouter>
   );
 }
 
