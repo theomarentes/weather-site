d7f4bba32537057a993ee5937bc04a2a424e9396
TheoMarentes <theokazmaros@gmail.com>
2023-12-13 08:52:46 +1000

    added days to forecast, added currentweather info, added more to forecast cards

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -7,28 +7,39 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 export function Forecast(){
 	let {weather, setWeather} = useContext(WeatherContext);
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    const d = new Date();
+    let day = d.getDay();
+    console.log(day)
 
 	if (weather.data) {
         return(
         <>
+        <h1>Forecast</h1>
         {weather.data.daily.map((element, index) => {
 				return (
+                    <>
+
+        
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
                         <Card.Body>
-                            <Card.Title>DAY NAME</Card.Title>
+                            <Card.Title>{days[(day+index) % 7]}</Card.Title>
                             <Card.Text>
-                                Min: {Math.round((element.temp.min - 273.15)) + "°C"}
-                                <br/>
-                                Max: {Math.round((element.temp.max - 273.15)) + "°C"}
-                            {console.log((element.temp.min))}
+                            {element.weather[0].main + " - " + element.weather[0].description}
+                                
+                                
+                            {console.log((element))}
                             </Card.Text>
                         </Card.Body>
                         <ListGroup className="list-group-flush">
-                            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                            <ListGroup.Item>Min: {Math.round((element.temp.min - 273.15)) + "°C"}</ListGroup.Item>
+                            <ListGroup.Item>Max: {Math.round((element.temp.max - 273.15)) + "°C"}</ListGroup.Item>
+                            <ListGroup.Item>Humidity: {Math.round((element.humidity)) + "%"}</ListGroup.Item>
+                        
                         </ListGroup>
                     </Card>
+                    </>
                 )
 		})}
         
